6175461ee92c8d3d0ca11c7cf4b144f180b2d9c1
pompomJ <67637461+pompomJ@users.noreply.github.com>
2024-09-26 19:33:49 +0900

    Update PythonApplication5.py
    コメント追加

--- PythonApplication5/PythonApplication5.py
+++ PythonApplication5/PythonApplication5.py
@@ -1,6 +1,6 @@
 # Pythonによる3つのデータの入力
 
-s = input().split()
-print(s[0])
-print(s[1])
-print(s[2])
+s = input().split() # 例えば入力されたのが"Hello"だとすると
+print(s[0]) # "H"が出力される
+print(s[1]) # "e"が出力される
+print(s[2]) # "l"が出力される
